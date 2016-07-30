b85a7e2b48bf9cec131472922c554f3b83aad22e
Cătălin Stan <catalin.stan@me.com>
2016-07-30 21:44:46 +0200

    Initial commit

---
+++ CSFeedKit/CSFeedKit.h
@@ -0,0 +1,19 @@
+//
+//  CSFeedKit.h
+//  CSFeedKit
+//
+//  Created by Cătălin Stan on 30/07/16.
+//  Copyright © 2016 Cătălin Stan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CSFeedKit.
+FOUNDATION_EXPORT double CSFeedKitVersionNumber;
+
+//! Project version string for CSFeedKit.
+FOUNDATION_EXPORT const unsigned char CSFeedKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSFeedKit/PublicHeader.h>
+
+
