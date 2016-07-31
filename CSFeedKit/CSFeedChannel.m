a82660bde0aa466f05f31a91969026749884d2b8
Cătălin Stan <catalin.stan@me.com>
2016-07-31 18:40:33 +0200

    Cleanup

---
+++ CSFeedKit/CSFeedChannel.m
@@ -0,0 +1,13 @@
+//
+//  CSFeedChannel.m
+//  CSFeedKit
+//
+//  Created by Cătălin Stan on 31/07/16.
+//  Copyright © 2016 Cătălin Stan. All rights reserved.
+//
+
+#import "CSFeedChannel.h"
+
+@implementation CSFeedChannel
+
+@end
